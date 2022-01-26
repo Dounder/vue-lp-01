58b91e9d931bda80b28ac794691e733aaf132004
dcode <dr.glasdou@gmail.com>
2022-01-26 10:05:22 -0600

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+    <main class="home">
+        <h1>home</h1>
+    </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'Home',
+});
+</script>
+
+<style lang="scss" scoped>
+.home {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    width: 100vw;
+}
+</style>
